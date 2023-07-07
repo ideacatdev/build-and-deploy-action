e8314feb181b62a1541a6bb9803055259889f0ad
ideacatlab <razvan@ideacat.ro>
2023-07-07 05:27:18 +0300

    okaiza

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ elif [ x$INPUT_BRANCH != x ]
 then
     caprover deploy --caproverUrl $INPUT_SERVER --appToken $INPUT_TOKEN --appName $INPUT_APP -b $INPUT_BRANCH
 else
-    caprover deploy --caproverUrl $INPUT_SERVER --appToken $INPUT_TOKEN --appName $INPUT_APP --tarFile ./deploy.tar
+    caprover deploy --caproverUrl $INPUT_SERVER --appToken $INPUT_TOKEN --appName $INPUT_APP --tarFile ./yolo.tar.gz
 fi
